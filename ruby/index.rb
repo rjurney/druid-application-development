008b86359ed8315946e801bdaba614d8761fdc48
Russell Jurney <russell.jurney@gmail.com>
2013-10-07 14:19:23 -0700

    Working Ruby version of app.

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,6 +1,18 @@
 # myapp.rb
 require 'sinatra'
+require 'druid'
+require 'json'
 
-get '/' do
-  'Hello world!'
+set :public_folder, File.dirname(__FILE__) + '/static'
+set :views, 'templates'
+
+client = Druid::Client.new('', {:static_setup => { 'realtime/wikipedia' => 'http://localhost:8083/druid/v2/' }})
+
+get '/timeseries' do
+  query = Druid::Query.new('realtime/wikipedia').double_sum(:added).granularity(:minute).interval("2013-01-01", "2014-01-01")
+  result = client.send(query)
+  jsonable = result.map {|r| {'timestamp' => r.timestamp, 'result' => r.row}}
+  @json = JSON.generate(jsonable)
+  puts @json
+  erb :index
 end
